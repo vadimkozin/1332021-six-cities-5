87a0835c919e9e287e794bfa13a99607b2798758
Vadim Kozin <ulin.salma@yandex.ru>
2020-10-21 15:45:36 +0300

    Добавит поддержку инструмента Redux DevTools

--- src/index.js
+++ src/index.js
@@ -11,8 +11,10 @@ const Settings = {
   NUMBER_RENTAL_OFFER: 312
 };
 
-const store = createStore(reducer);
-console.log(store);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
     <Provider store={store}>
